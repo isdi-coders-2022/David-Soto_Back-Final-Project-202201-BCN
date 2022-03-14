c9461b401db45ef116ebec3f79d0369dbc8d6252
David Soto <davidsotosendarrubias@gmail.com>
2022-03-14 09:47:25 +0100

    Created heroRouters with test

---
+++ src/server/routers/heroRouters.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const getAllHeroes = require("../controllers/heroController");
+
+const router = express();
+
+router.get("/listAll", getAllHeroes);
+
+module.exports = router;
