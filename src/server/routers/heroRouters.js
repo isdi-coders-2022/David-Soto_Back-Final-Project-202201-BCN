f43fb5d5d4ea9e783d3e115422385f1f33bfb138
David Soto <davidsotosendarrubias@gmail.com>
2022-03-18 12:42:06 +0100

    Updated heroRouters with delete and create

--- src/server/routers/heroRouters.js
+++ src/server/routers/heroRouters.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const getAllHeroes = require("../controllers/heroController");
+const {
+  getAllHeroes,
+  createHero,
+  deleteHero,
+} = require("../controllers/heroController");
 
 const router = express();
 
 router.get("/listAll", getAllHeroes);
+router.post("/createNew", createHero);
+router.post("/deleteHero/:id", deleteHero);
 
 module.exports = router;
