ca01e1c7b262d610fd8092e3bbda26241712ecc4
David Soto <davidsotosendarrubias@gmail.com>
2022-03-21 11:47:16 +0100

    UserController added bcrypt and token

--- src/db/models/UserModel.js
+++ src/db/models/UserModel.js
@@ -3,8 +3,8 @@ const { model, Schema } = require("mongoose");
 const UserModel = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  favorite: { type: String },
-  friends: { type: String },
+  favoriteHeroes: { type: Object },
+  createdHeroes: { type: Object },
 });
 
 const User = model("User", UserModel, "users");
