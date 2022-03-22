9405633039e20a8b3a03729abc9379133c7707f0
David Soto <davidsotosendarrubias@gmail.com>
2022-03-22 09:14:33 +0100

    added authentication

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const heroRouter = require("./routers/heroRouters");
 const userRouter = require("./routers/userRouters");
+const authenticateToken = require("./middlewares/authentication");
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
-app.use("/hero", heroRouter);
 app.use("/user", userRouter);
+app.use("/hero", authenticateToken, heroRouter);
 
 module.exports = app;
